d22cf79f1c3a364c1705b71b9aa43aa279b4a961
Calingaram1979 <00126223@uca,edu,sv>
2023-07-03 20:21:32 -0600

    :construction:MenuPrincipal

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -19,7 +19,31 @@ int main(){
 }
 void MenuPrincipal()
 {
+    int opcion;
+    string opcion1;
+    string contra;
+    string nombre;
+    string NombreAdmin = "usuario";
+    string ContraAdmin = "ABC123";
+    cout << " 1) Usuario Administrador.\n 2) Usuario General.\n";
+    cout << "Elija una opcion:\n";
+    cin >> opcion;
 
+    switch (opcion)
+    {
+    case 1:
+        cout << "Ingrese nombre de administrador:" << endl;
+        cin >> nombre;
+        cout << "Ingrese la clave:" << endl;
+        cin >> contra;
+        break;
+    case 2:
+    break;
+    case 3:
+    break;
+    default:
+        break;
+    }
 }
 void MenuAdministrador()
 {
@@ -35,5 +59,5 @@ void Limpiar()
 }
 void Slogan()
 {
-    
+
 }
