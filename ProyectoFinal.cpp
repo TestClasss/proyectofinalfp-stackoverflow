97f194cad8a5d04f1ae9e097d86288cb70bd34e3
Calingaram1979 <00126223@uca,edu,sv>
2023-07-03 20:13:56 -0600

    :ambulance:Menus

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
+
+void MenuPrincipal();
+void MenuAdministrador();
+void MenuUsuario();
+void Limpiar();
+void Slogan();
 
 int main(){
 
 
 
     return 0;
+}
+void MenuPrincipal()
+{
+
+}
+void MenuAdministrador()
+{
+
+}
+void MenuUsuario()
+{
+
+}
+void Limpiar()
+{
+
+}
+void Slogan()
+{
+    
 }
